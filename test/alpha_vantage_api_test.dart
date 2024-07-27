945b3d6ceebdcc80a4e6b061e14fe162cee938a6
Ivo Fernandes <ivomuze@gmail.com>
2024-07-27 09:43:00 +0100

    Test for alpha vantage basic service

--- test/alpha_vantage_api_test.dart
+++ test/alpha_vantage_api_test.dart
@@ -1,8 +1,45 @@
+import 'package:alpha_vantage_api/services/complete_alpha_vantage_service.dart';
+import 'package:alpha_vantage_api/services/mixins/alpha_vantage_service.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:alpha_vantage_api/alpha_vantage_api.dart';
+import 'configuration_environment.dart';
+
+class RealAlphaVantageService with AlphaVantageService {}
 
 void main() {
-  test('adds one to input values', () {
+  group('AlphaVantageService Real API Tests', () {
+    late CompleteAlphaVantageService service;
+    late String apiKey;
+
+    setUp(() {
+      service = CompleteAlphaVantageService(ConfigurationEnvironment.API_KEY);
+    });
+
+    test('fetchContent fetches real data', () async {
+      final functionName = 'TIME_SERIES_INTRADAY';
+      final additionalParams = {'symbol': 'IBM', 'interval': '5min'};
+
+      try {
+        final content = await service.fetchContent(functionName, additionalParams: additionalParams);
+        print('Received content: $content');
+
+        expect(content.isNotEmpty, isTrue);
+      } catch (e) {
+        fail('Failed to fetch data: $e');
+      }
+    });
+
+    test('EARNINGS_CALENDAR request', () async {
+      final functionName = 'EARNINGS_CALENDAR';
+
+      try {
+        // The additional parameters for the EARNINGS_CALENDAR request can vary
+        final content = await service.fetchContent(functionName);
+
+        expect(content.isNotEmpty, isTrue);
+      } catch (e) {
+        print('Caught expected exception: $e');
+      }
+    });
   });
 }
---
+++ test/configuration_environment.dart
@@ -0,0 +1,4 @@
+class ConfigurationEnvironment {
+  static const String API_KEY = '<Put your API key here>';
+
+}
